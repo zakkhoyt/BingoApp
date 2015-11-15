0012f2aba59272cf078c97879da780ff8ec03f46
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-11-14 21:00:20 -0800

    first commit

---
+++ BingoApp/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  BingoApp
+//
+//  Created by Zakk Hoyt on 11/14/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+import UIKit
+
+class Game: NSObject {
+
+}
---
+++ BingoApp/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  BingoApp
+//
+//  Created by Zakk Hoyt on 11/14/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+}
